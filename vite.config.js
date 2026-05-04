b28e184d3e1bca1d3628afd56a6555bfcd0a0265
daorga <daorga@gmail.com>
2026-05-04 16:17:48 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/facturapido/',   // debe coincidir con el nombre de tu repo en GitHub
+  base: '/Facturapido/',   // debe coincidir con el nombre de tu repo en GitHub
 })
