0a0373d6b43e851ce6171f4a949014af88908434
daorga <daorga@gmail.com>
2026-05-04 16:23:16 -0600

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://wiquxjudclhbfhxqysry.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpcXV4anVkY2xoYmZoeHF5c3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MjM2ODksImV4cCI6MjA5MzQ5OTY4OX0.EPRDEg6wM6X19oEftJHCjHHT3PkGhopkoJlqQMeIK_U'
 )
